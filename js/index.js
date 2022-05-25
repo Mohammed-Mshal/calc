8e73be0cc6062ef0ea6394650c377ee1903dd5ab
Mohammed-Mshal <83920083+Mohammed-Mshal@users.noreply.github.com>
2022-05-25 19:51:59 +0300

    First Upload

---
+++ js/index.js
@@ -0,0 +1,30 @@
+let btnCalc = document.querySelector(`.btnCalc`);
+let result = document.querySelector(`p.result`);
+document.querySelector(`.btn-date`).style.display = `none`;
+
+btnCalc.addEventListener(`click`, function () {
+  let totalUni = 0;
+  document.querySelectorAll(`.uni input`).forEach((e) => {
+    totalUni += +e.value;
+  });
+  if (totalUni == 100) {
+    totalUni = 0;
+    for (let i = 0; i < 3; i++) {
+      totalUni +=
+        (+document.querySelectorAll(`.uni input`)[i].value / 100) *
+        +document.querySelectorAll(`.degree input`)[i].value;
+    }
+    result.innerHTML = `نسبتك الموزونة هي ${totalUni}%
+    <br>
+    <span style="color:black;font-size:14px">بالتوفيق ❤️</span>`;
+    result.style.color = `#005950`;
+    result.style.fontSize = `28px`;
+    document.querySelector(`.btn-date`).style.display = `inline`;
+  } else {
+    document.querySelector(`.btn-date`).style.display = `none`;
+    result.innerHTML = `النسب المحددة من الجامعة يجب أن يكون مجموعها = 100    `;
+    result.style.color = `red`;
+    result.style.fontSize = `28px`;
+    result.style.backgroundColor = `white`;
+  }
+});
